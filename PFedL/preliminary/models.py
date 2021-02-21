379fcbee835ebad4106ba7c05447a6c682e0914d
Conley <40120092+NeoDH@users.noreply.github.com>
2021-02-21 20:30:55 +0800

    Create models.py

---
+++ PFedL/preliminary/models.py
@@ -0,0 +1,30 @@
+
+import torch
+import torch.nn.functional as F
+
+class ConvNet(torch.nn.Module):
+    def __init__(self):
+        super(ConvNet, self).__init__()
+        self.conv1 = torch.nn.Conv2d(1, 6, 5)
+        self.pool = torch.nn.MaxPool2d(2, 2)
+        self.conv2 = torch.nn.Conv2d(6, 16, 5)
+        self.fc1 = torch.nn.Linear(16 * 4 * 4, 62)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 4 * 4)
+        x = self.fc1(x)
+        return x
+
+class Net(torch.nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = torch.nn.Linear(784,100)
+        self.fc2 = torch.nn.Linear(100, 10)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
